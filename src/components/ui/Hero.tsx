56d2a5f89b8a098774b3097a8d07ae009cb4a39e
Yar Zar <khunyazar@aceplussolutions.com>
2025-02-04 14:17:37 +0630

    seventh commit

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -29,11 +29,12 @@ export function Hero() {
     image4.src,
   ];
 
-  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
-  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
-  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
-  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
-  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.5], [0, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.5], [0, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.5], [0, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.5], [0, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.5], [0, 1.2]);
+
 
   return (
     <div className="h-[400vh] w-full relative" ref={ref}>
